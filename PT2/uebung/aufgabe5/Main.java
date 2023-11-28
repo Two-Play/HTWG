b0ec1df700c6f6882d9915a6d424f32730aab528
Philippe <philippe.westenfelder@htwg-konstanz.de>
2023-11-28 17:41:27 +0100

    der final-(Salz)streuer ist leer (PT2 a5)

--- PT2/uebung/aufgabe5/Main.java
+++ PT2/uebung/aufgabe5/Main.java
@@ -4,10 +4,13 @@ import java.awt.*;
 
 public final class Main {
 
-    final static int DELTA = 30;
-    final static double EPSILON = 0.01;
-    final static double GREENPOINT = 0.01;
+    // Abbr. Wert
+    final static double EPSILON = 0.001;
 
+    // Wert der seitenlänge w, ab der mit grün gemalt wird
+    final static double GREENPOINT = 0.001;
+
+    // Def. Farben
     final static Color BROWN = new Color(120, 66, 3);
     final static Color GREEN = new Color(84,203, 19);
 
@@ -16,51 +19,113 @@ public final class Main {
 
         StdDraw.setPenRadius(0.003);
         StdDraw.clear();
-        StdDraw.setCanvasSize(800,800);
+        StdDraw.setCanvasSize(1000,1000);
 
-        pyTree1();
-
-        //StdDraw.line(0,0,1,1);
-
+        pyTree1(0.6, 0.05, 0, 0.1);
+        //pyTree2(0.5, 0.05, 0, 0.05);
 
         //Show img
         StdDraw.show(0);
     }
 
 
-    private static void pyTree1() {
-        double w = 0.1;
+    private static void pyTree1(double x, double y, double gamma, double w) {
 
-        double xEndpointLeft = 0.5 - w/2;
-        double yEndpointLeft = w;
+        // Konstanten relativen Neigungswinkel
+        final double DELTA = 0.4;
 
-        double xEndpointRight = 0.5 + w/2;
-        double yEndpointRight = w;
+        // Setze Stift Farbe
+        if (w < GREENPOINT) {
+            StdDraw.setPenColor(GREEN);
+        } else {
+            StdDraw.setPenColor(BROWN);
+        }
 
-        StdDraw.setPenColor(BROWN);
-
-        StdDraw.line(0.5 - w/2,0, xEndpointLeft, yEndpointLeft);
-        StdDraw.line(0.5 + w/2,0, xEndpointRight, yEndpointRight);
-
+        // Punkte, Strecke und Winkel berechnen
         final double u = w * Math.cos(DELTA);
+        final double v = w * Math.sin(DELTA);
 
-        final double s = w * Math.sin(DELTA);
-        final double c = w * Math.cos(DELTA);
-        final double Cx = xEndpointLeft + c - s;
-        final double Cy = yEndpointLeft + c + s;
-        final double Dx = xEndpointRight - s;
-        final double Dy = yEndpointLeft + s;
-        //final double D = xEndpointLeft-
+        final double s = w * Math.sin(gamma);
+        final double c = w * Math.cos(gamma);
 
-        StdDraw.line(xEndpointLeft, yEndpointLeft, Dx, Dy);
-        StdDraw.line(xEndpointRight, yEndpointRight, Cx, Cy );
+        final double xB = x + c;
+        final double yB = y + s;
 
-        xEndpointLeft = Cx;
-        yEndpointLeft = Cy;
+        final double xC = x + c - s;
+        final double yC = y + s + c;
 
-        w = u;
+        final double xD = x - s;
+        final double yD = y + c;
 
+        final double xE = xD + u * Math.cos(gamma + DELTA);
+        final double yE = yD + u * Math.sin(gamma + DELTA);
+
+        // Linien Zeichnen
+        // B → C
+        StdDraw.line(xB, yB, xC, yC);
+        // A → D
+        StdDraw.line(x, y, xD, yD);
+
+        // Abbr. bedingung (bzw. Aufrufbedingung)
+        if (w > EPSILON) {
+            // großes Rechteck
+            pyTree1(xD, yD, gamma + DELTA, u);
+            // kleines Rechteck
+            pyTree1(xE, yE, gamma + DELTA - 1.5, v);
+        }
     }
 
+
+    private static void pyTree2(double x, double y, double gamma, double w) {
+
+        final double height = Math.random() * w + w;
+        // Konstanten relativen Neigungswinkel
+        final double DELTA = Math.toRadians(Math.random() * 60 + 15);
+
+        if (w < GREENPOINT) {
+            StdDraw.setPenColor(GREEN);
+        } else {
+            StdDraw.setPenColor(BROWN);
+        }
+
+        final double s = w * Math.sin(gamma);
+        final double c = w * Math.cos(gamma);
+        //
+        final double r = height * Math.sin(gamma);
+        final double h = height * Math.cos(gamma);
+
+        // Punkt B
+        final double xB = x + c;
+        final double yB = y + s;
+
+        // Punkt C
+        final double xC = x + c - r;
+        final double yC = y + s + h;
+
+        // Punkt D
+        final double xD = x - r;
+        final double yD = y + h;
+
+        final double u = w * Math.cos(DELTA);
+        final double v = w * Math.sin(DELTA);
+
+        // Punkt E
+        final double xE = xD + u * Math.cos(gamma + DELTA);
+        final double yE = yD + u * Math.sin(gamma + DELTA);
+
+        // Linien Zeichnen
+        // B → C
+        StdDraw.line(xB, yB, xC, yC);
+        // A → D
+        StdDraw.line(x, y, xD, yD);
+
+        // Abbr. bedingung (bzw. Aufrufbedingung)
+        if (w > EPSILON) {
+            // großes Rechteck
+            pyTree2(xD, yD, gamma + DELTA, u);
+            // kleines Rechteck
+            pyTree2(xE, yE, gamma + DELTA - 1.5, v);
+        }
+    }
 
 }
